bfb7dbf599f9e51230d45b89d1117abd025b14c5
Enejo James <spec.en.james@gmail.com>
2018-06-13 15:33:44 +0100

    Link result page

--- static-files/js/thankyou.js
+++ static-files/js/thankyou.js
@@ -1,4 +1,4 @@
 $(document).ready(() => {
     setTimeout(() => $('.content-wrapper').css('top', '50%'), 250);
-    $('#view').click(() => location.href = 'https://enjames.github.io/vlapma/views/report.html')
+    $('#view').click(() => location.href = 'https://enjames.github.io/vlapma/views/result.html')
 });
